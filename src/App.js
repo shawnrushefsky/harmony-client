47ef8894be251e3e919e526ad09932b9909a7298
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2019-04-19 17:50:50 -0500

    input box

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TopBar from './TopBar';
+import InputBox from './InputBox';
+
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <CssBaseline>
           <TopBar />
+          <InputBox />
         </CssBaseline>
       </React.Fragment>
     );
